6e51a4ad955de00aac0413d7d16780b287b1bfa0
LeNgocHoa <hoavimaru68@gmail.com>
2017-04-24 16:39:00 +0700

    test2

--- app/src/main/java/com/example/lengochoa/toeic/AnswerPart1.java
+++ app/src/main/java/com/example/lengochoa/toeic/AnswerPart1.java
@@ -1,57 +1,87 @@
 package com.example.lengochoa.toeic;
 
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
 
-import org.w3c.dom.Text;
 
 public class AnswerPart1 extends AppCompatActivity {
 
-    private String[] answerSheet;
-    private String[] keys;
+
     private RadioButton a,b,c,d;
     private Button next,pre;
     private TextView txtnoq;
     private int curans;
-
-    private void InitKey(){
-        keys = new String[100];
-        for(int i = 0;i<100;i++){
-            keys[i] = "A";
-        }
-    }
-
-    private void SetAns(){
-        Question q = new Question();
-        String[] answer = q.getAnswer();
-        a.setText(answer[0]);
-        b.setText(answer[1]);
-        c.setText(answer[2]);
-        d.setText(answer[3]);
-    }
+    private VideoView videoView;
+    private int pos;
+    private MediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer_part1);
-        InitKey();
 
-        Intent intent = getIntent();
-        answerSheet = intent.getStringArrayExtra("answerSheet");
+        curans = 0;
+//        Intent intent = getIntent();
+//        answerSheet = intent.getStringArrayExtra("answerSheet");
+//        keys = intent.getStringArrayExtra("ansKey");
+//        audios= intent.getStringArrayExtra("audios");
+//        indexTest = intent.getIntExtra("indexTest",1);
+//        Bundle bundle = getIntent().getExtras();
+//        questions = (ArrayList<Question>)bundle.getSerializable("questions");
+
+        videoView = (VideoView)findViewById(R.id.videoView);
+        //Tao media Controller
+        if(mediaController == null){
+            mediaController = new MediaController(AnswerPart1.this);
+            mediaController.setAnchorView(videoView);
+            videoView.setMediaController(mediaController);
+        }
+
+        try{
+            videoView.setVideoPath("file:///android_asset/audio1/a01.mp3");
+            videoView.requestFocus();
+            videoView.start();
+            pos = 0 ;
+        }catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                videoView.seekTo(pos);
+                if(pos == 0){
+                    videoView.start();
+                }
+                mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mediaPlayer, int i, int i1) {
+                        mediaController.setAnchorView(videoView);
+                    }
+                });
+            }
+        });
+
 
         a = (RadioButton)findViewById(R.id.rdba);
         b = (RadioButton)findViewById(R.id.rdbb);
         c = (RadioButton)findViewById(R.id.rdbc);
         d = (RadioButton)findViewById(R.id.rdbd);
 
-        curans = 0;
         txtnoq = (TextView)findViewById(R.id.txtnoq);
         pre = (Button)findViewById(R.id.btnPre);
         next = (Button)findViewById((R.id.btnNext));
@@ -61,7 +91,7 @@ public class AnswerPart1 extends AppCompatActivity {
             public void onClick(View v) {
                 if(curans<9){
                     curans++;
-                    txtnoq.setText(curans+"/10");
+                    txtnoq.setText(curans+1+"/10");
                 }
             }
         });
@@ -71,33 +101,10 @@ public class AnswerPart1 extends AppCompatActivity {
             public void onClick(View v) {
                 if(curans>0){
                     curans--;
-                    txtnoq.setText(curans+"/10");
+                    txtnoq.setText(curans+1+"/10");
                 }
             }
         });
 
-        if(keys[curans].equals("A")) a.setTextColor(Color.GREEN);
-        else if(keys[curans].equals("B")) b.setTextColor(Color.GREEN);
-        else if(keys[curans].equals("C")) c.setTextColor(Color.GREEN);
-        else d.setTextColor(Color.GREEN);
-
-        if(!answerSheet[curans].equals(keys[curans])){
-            if(answerSheet[curans].equals("A")) {
-                a.setChecked(true);
-                a.setTextColor(Color.RED);
-            }
-            else if(answerSheet[curans].equals("B")){
-                b.setChecked(true);
-                b.setTextColor(Color.RED);
-            }
-            else if(answerSheet[curans].equals("C")) {
-                c.setChecked(true);
-                c.setTextColor(Color.RED);
-            }
-            else {
-                d.setChecked(true);
-                d.setTextColor(Color.RED);
-            }
         }
-    }
 }
