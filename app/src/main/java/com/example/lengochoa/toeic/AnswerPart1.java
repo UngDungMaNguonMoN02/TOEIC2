dca54d53fe8fbd8c8deff69e678b54e4776ebfdf
LeNgocHoa <hoavimaru68@gmail.com>
2017-05-02 11:34:39 +0700

    bản toeic sửa đổi sau khi add và chỉnh sửa lại mediaplayer (hòa) lần 9

--- app/src/main/java/com/example/lengochoa/toeic/AnswerPart1.java
+++ app/src/main/java/com/example/lengochoa/toeic/AnswerPart1.java
@@ -2,7 +2,9 @@ package com.example.lengochoa.toeic;
 
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +17,7 @@ import android.widget.ImageSwitcher;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
@@ -31,18 +34,20 @@ public class AnswerPart1 extends AppCompatActivity {
     private int indexTest;
     private ArrayList<Question> questions;
 
-    private Button btnPlay, btnPause, btnForward, btnRewind;
+    private Button btnPlay;
+//            btnPause, btnForward, btnRewind;
     private MediaPlayer mediaPlayer;
 
     private double startTime = 0;
     private double finalTime = 0;
 
     private Handler m_Handler = new Handler();
-    private int forwardTime = 5000;
-    private int backwardTime = 5000;
+//    private int forwardTime = 5000;
+//    private int backwardTime = 5000;
     private SeekBar seekbar;
     private TextView txtTimeAudio;
     private Button btnMenu;
+    private boolean run;
 
     private ImageSwitcher imageSwitcher;
 
@@ -60,33 +65,49 @@ public class AnswerPart1 extends AppCompatActivity {
         b.setTextColor(Color.BLACK);
         c.setTextColor(Color.BLACK);
         d.setTextColor(Color.BLACK);
-        if(keys[curans].equals("A")) a.setTextColor(Color.rgb(95,139,101));
-        else if(keys[curans].equals("B")) b.setTextColor(Color.rgb(95,139,101));
-        else if(keys[curans].equals("C")) c.setTextColor(Color.rgb(95,139,101));
-        else d.setTextColor(Color.rgb(95,139,101));
+        switch (answerSheet[curans]){
+            case "A": a.setChecked(true);break;
+            case "B": b.setChecked(true);break;
+            case "C": c.setChecked(true);break;
+            case "D": d.setChecked(true);break;
+            default:break;
+        }
 
         if(!answerSheet[curans].equals(keys[curans])){
-            if(answerSheet[curans].equals("A")) {
-                a.setChecked(true);
-                a.setText(a.getText() + " (Sai)");
-                a.setTextColor(Color.rgb(188,42,70));
-            }
-            else if(answerSheet[curans].equals("B")){
-                b.setChecked(true);
-                b.setText(b.getText() + " (Sai)");
-                b.setTextColor(Color.rgb(188,42,70));
-            }
-            else if(answerSheet[curans].equals("C")) {
-                c.setChecked(true);
-                c.setText(c.getText() + " (Sai)");
-                c.setTextColor(Color.rgb(188,42,70));
-            }
-            else {
-                d.setChecked(true);
-                d.setText(d.getText() + " (Sai)");
-                d.setTextColor(Color.rgb(188,42,70));
+            switch (answerSheet[curans]){
+                case "A":
+                    a.setText(a.getText() + getString(R.string.wrong));
+                    a.setTextColor(Color.rgb(188,42,70));break;
+                case "B":
+                    b.setText(b.getText() + getString(R.string.wrong));
+                    b.setTextColor(Color.rgb(188,42,70));break;
+                case "C":
+                    c.setText(c.getText() + getString(R.string.wrong));
+                    c.setTextColor(Color.rgb(188,42,70));break;
+                case "D":
+                    d.setText(d.getText() + getString(R.string.wrong));
+                    d.setTextColor(Color.rgb(188,42,70));break;
+                default:break;
             }
         }
+        else{
+            switch (keys[curans]){
+                case"A":
+                    a.setText(a.getText() + getString(R.string.right));
+                    a.setTextColor(Color.rgb(95,139,101));break;
+                case "B":
+                    b.setText(a.getText() + getString(R.string.right));
+                    b.setTextColor(Color.rgb(95,139,101));break;
+                case "C":
+                    c.setText(a.getText() + getString(R.string.right));
+                    c.setTextColor(Color.rgb(95,139,101));break;
+                case "D":
+                    d.setText(a.getText() + getString(R.string.right));
+                    d.setTextColor(Color.rgb(95,139,101));break;
+                default:break;
+            }
+        }
+
         imageSwitcher.setBackgroundResource(getResIdByName("t" + indexTest + "img" + (curans+1)));
         Animation in= AnimationUtils.loadAnimation(AnswerPart1.this, android.R.anim.slide_in_left);
         in.setDuration(1000);
@@ -95,6 +116,7 @@ public class AnswerPart1 extends AppCompatActivity {
 
     private void startAudio(){
         mediaPlayer = new MediaPlayer();
+        run = true;
         try
         {
             AssetFileDescriptor afd = getAssets().openFd("audio"+indexTest +"/"+audios[curans]);
@@ -122,8 +144,8 @@ public class AnswerPart1 extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
-        btnPlay.setEnabled(true);
-        btnPause.setEnabled(false);
+//        btnPlay.setEnabled(true);
+//        btnPause.setEnabled(false);
     }
 
     @Override
@@ -137,9 +159,9 @@ public class AnswerPart1 extends AppCompatActivity {
         imageSwitcher.startAnimation(in);
 
         btnPlay = (Button) findViewById(R.id.btnStart);
-        btnPause = (Button) findViewById(R.id.btnPause);
-        btnForward = (Button) findViewById(R.id.btnForward);
-        btnRewind = (Button) findViewById(R.id.btnRewind);
+//        btnPause = (Button) findViewById(R.id.btnPause);
+//        btnForward = (Button) findViewById(R.id.btnForward);
+//        btnRewind = (Button) findViewById(R.id.btnRewind);
         btnMenu = (Button) findViewById(R.id.btnMenu);
         txtTimeAudio = (TextView)findViewById(R.id.txtTimeAudio);
 
@@ -153,55 +175,75 @@ public class AnswerPart1 extends AppCompatActivity {
 
         seekbar = (SeekBar)findViewById(R.id.seekBar);
 
+        final Runnable updateSongTime = new Runnable() {
+            @Override
+            public void run() {
+                startTime = mediaPlayer.getCurrentPosition();
+                txtTimeAudio.setText(String.format("%d s", TimeUnit.MILLISECONDS.toSeconds((long) startTime)));
+                seekbar.setProgress((int)startTime);
+                m_Handler.postDelayed(this, 100);
+            }
+        };
+
         btnPlay.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                mediaPlayer.start();
-                m_Handler.postDelayed(UpdateSongTime,100);
-                btnPause.setEnabled(true);
-                btnPlay.setEnabled(false);
-            }
-        });
+                if(run){
+                    mediaPlayer.start();
+                    m_Handler.postDelayed(updateSongTime,100);
+//                btnPause.setEnabled(true);
+//                    btnPlay.setEnabled(false);
+                    Toast.makeText(getApplicationContext(),"Play",Toast.LENGTH_SHORT).show();
+                    run=false;
 
-        btnPause.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                mediaPlayer.pause();
-                btnPause.setEnabled(false);
-                btnPlay.setEnabled(true);
-            }
-        });
-
-        btnForward.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                int temp = (int)startTime;
-                if((temp+forwardTime)<=finalTime)
-                {
-                    startTime = startTime + forwardTime;
-                    mediaPlayer.seekTo((int) startTime);
+                }
+                else{
+                    mediaPlayer.pause();
+                    Toast.makeText(getApplicationContext(),"Pause",Toast.LENGTH_SHORT).show();
+                    run=true;
                 }
             }
         });
-        btnRewind.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                int temp = (int)startTime;
 
-                if((temp-backwardTime)>0) {
-                    startTime = startTime - backwardTime;
-                    mediaPlayer.seekTo((int) startTime);
-                }
-            }
-        });
+//        btnPause.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                mediaPlayer.pause();
+//                btnPause.setEnabled(false);
+//                btnPlay.setEnabled(true);
+//            }
+//        });
+
+//        btnForward.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                int temp = (int)startTime;
+//                if((temp+forwardTime)<=finalTime)
+//                {
+//                    startTime = startTime + forwardTime;
+//                    mediaPlayer.seekTo((int) startTime);
+//                }
+//            }
+//        });
+//        btnRewind.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                int temp = (int)startTime;
+//
+//                if((temp-backwardTime)>0) {
+//                    startTime = startTime - backwardTime;
+//                    mediaPlayer.seekTo((int) startTime);
+//                }
+//            }
+//        });
 
         a = (RadioButton)findViewById(R.id.rdba);
         b = (RadioButton)findViewById(R.id.rdbb);
@@ -246,20 +288,12 @@ public class AnswerPart1 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mediaPlayer.release();
-                m_Handler.removeCallbacks(UpdateSongTime);
+                m_Handler.removeCallbacks(updateSongTime);
                 Intent menu = new Intent(AnswerPart1.this,Menu.class);
-                startActivity(menu);
+                menu.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                finish();
             }
         });
     }
 
-    private Runnable UpdateSongTime = new Runnable()
-    {
-        public void run() {
-            startTime = mediaPlayer.getCurrentPosition();
-            txtTimeAudio.setText(String.format("%d s", TimeUnit.MILLISECONDS.toSeconds((long) startTime)));
-            seekbar.setProgress((int)startTime);
-            m_Handler.postDelayed(this, 100);
-        }
-    };
 }
--- app/src/main/java/com/example/lengochoa/toeic/Menu.java
+++ app/src/main/java/com/example/lengochoa/toeic/Menu.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,71 +21,81 @@ public class Menu extends AppCompatActivity {
     private int point;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        final Bundle bundle= getIntent().getExtras();
-        questions = (ArrayList<Question>)bundle.getSerializable("questions");
+            final Bundle bundle = getIntent().getExtras();
+            questions = (ArrayList<Question>) bundle.getSerializable("questions");
 
-        Intent intent = getIntent();
-        answerSheet = intent.getStringArrayExtra("answerSheet");
-        audios = intent.getStringArrayExtra("audios");
-        indexTest = intent.getIntExtra("indexTest",1);
-        ansKey = new String[100];
-        int i = 0;
-        for(Question q : questions){
-            ansKey[i] = q.getKey();
-            i++;
+            Intent intent = getIntent();
+            answerSheet = intent.getStringArrayExtra("answerSheet");
+            audios = intent.getStringArrayExtra("audios");
+            indexTest = intent.getIntExtra("indexTest", 1);
+            ansKey = new String[100];
+            int i = 0;
+            for (Question q : questions) {
+                ansKey[i] = q.getKey();
+                i++;
+            }
+            point = 0;
+            for (int j = 0; j < 100; j++) {
+                if (answerSheet[j].equals(ansKey[j])) point++;
+            }
+
+
+            txtPoint = (EditText) findViewById(R.id.txtPoint);
+            txtPoint.setText("Bạn đúng " + point + " câu/100");
+
+
+        try {
+            final Button btnAnswerPart1 = (Button) findViewById(R.id.btnPart1);
+            btnAnswerPart1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent AnswerPart1 = new Intent(Menu.this, AnswerPart1.class);
+                    AnswerPart1.putExtra("answerSheet", answerSheet);
+                    AnswerPart1.putExtra("ansKey", ansKey);
+                    AnswerPart1.putExtra("audios", audios);
+                    AnswerPart1.putExtra("indexTest", indexTest);
+                    bundle.putSerializable("questions", questions);
+                    AnswerPart1.putExtras(bundle);
+                    startActivity(AnswerPart1);
+                }
+            });
+
+        }catch (Exception e){
+            e.getMessage();
+            Toast.makeText(Menu.this,e.getMessage(), Toast.LENGTH_LONG).show();
         }
-        point = 0;
-        for(int j  = 0 ; j < 100 ; j++){
-            if(answerSheet[j].equals(ansKey[j])) point++;
-        }
+            final Button btnPart2 = (Button) findViewById(R.id.btnPart2);
+            btnPart2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent AnswerPart2 = new Intent(Menu.this, AnswerPart2.class);
+                    AnswerPart2.putExtra("answerSheet", answerSheet);
+                    AnswerPart2.putExtra("ansKey", ansKey);
+                    AnswerPart2.putExtra("audios", audios);
+                    AnswerPart2.putExtra("indexTest", indexTest);
+                    bundle.putSerializable("questions", questions);
+                    AnswerPart2.putExtras(bundle);
+                    startActivity(AnswerPart2);
+                }
+            });
+            final Button btnPart3 = (Button) findViewById(R.id.btnPart3);
+            btnPart3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
-        txtPoint = (EditText)findViewById(R.id.txtPoint);
-        txtPoint.setText("Bạn đúng " + point + " câu/100");
+                }
+            });
+            final Button btnPart4 = (Button) findViewById(R.id.btnPart4);
+            btnPart4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
-        final Button btnAnswerPart1 = (Button)findViewById(R.id.btnPart1);
-        btnAnswerPart1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent AnswerPart1 = new Intent(Menu.this,AnswerPart1.class);
-                AnswerPart1.putExtra("answerSheet",answerSheet);
-                AnswerPart1.putExtra("ansKey",ansKey);
-                AnswerPart1.putExtra("audios",audios);
-                AnswerPart1.putExtra("indexTest",indexTest);
-                bundle.putSerializable("questions",questions);
-                AnswerPart1.putExtras(bundle);
-                startActivity(AnswerPart1);
-            }
-        });
-        final Button btnPart2 = (Button)findViewById(R.id.btnPart2);
-        btnPart2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent AnswerPart2 = new Intent(Menu.this,AnswerPart2.class);
-                AnswerPart2.putExtra("answerSheet",answerSheet);
-                AnswerPart2.putExtra("ansKey",ansKey);
-                AnswerPart2.putExtra("audios",audios);
-                AnswerPart2.putExtra("indexTest",indexTest);
-                bundle.putSerializable("questions",questions);
-                AnswerPart2.putExtras(bundle);
-                startActivity(AnswerPart2);
-            }
-        });
-        final Button btnPart3 = (Button)findViewById(R.id.btnPart3);
-        btnPart3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                }
+            });
 
-            }
-        });
-        final Button btnPart4 = (Button)findViewById(R.id.btnPart4);
-        btnPart4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 }
